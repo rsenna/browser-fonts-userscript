1d81c3224b6c3e5cc8ea30f3bdf65a7f6bee8402
Rogério Senna [mb13] <rsennac@outlook.com>
2025-02-13 14:21:44 +0100

    make CSS rules be mutually exclusive

--- set-fonts.user.js
+++ set-fonts.user.js
@@ -14,66 +14,94 @@
 //
 const proportional = "'Noto Sans Condensed','Open Sans',sans-serif";
 const monospace =
-  "'VictorMono Nerd Font','Hurmit Nerd Font',Consolas,'Courier New',monospace \
-  !important;font-size:0.75rem;line-height:1.5";
+  "'VictorMono Nerd Font','Hurmit Nerd Font',Consolas,'Courier New',monospace !important;font-size:0.75rem;line-height:1.5";
+
+const innerHTML = `:not(textarea),:not(textarea):first-child,:not(textarea):last-child *,:not(textarea):first-child,\
+:not(textarea):first-child:first-child,:not(textarea):first-child:last-child *,:not(textarea):last-child *,\
+:not(textarea):last-child *:first-child,:not(textarea):last-child *:last-child *,:not(textarea):first-child,\
+:not(textarea):first-child:first-child,:not(textarea):first-child:last-child *,:not(textarea):first-child:first-child,\
+:not(textarea):first-child:first-child:first-child,:not(textarea):first-child:first-child:last-child *,\
+:not(textarea):first-child:last-child *,:not(textarea):first-child:last-child *:first-child,\
+:not(textarea):first-child:last-child *:last-child *,:not(textarea):last-child *,:not(textarea):last-child *:first-child,\
+:not(textarea):last-child *:last-child *,:not(textarea):last-child *:first-child,:not(textarea):last-child *:first-child:first-child,\
+:not(textarea):last-child *:first-child:last-child *,:not(textarea):last-child *:last-child *,:not(textarea):last-child *:last-child *:first-child,\
+:not(textarea):last-child *:last-child *:last-child *,:not(code),:not(code):first-child,:not(code):last-child *,:not(code):first-child,\
+:not(code):first-child:first-child,:not(code):first-child:last-child *,:not(code):last-child *,:not(code):last-child *:first-child,\
+:not(code):last-child *:last-child *,:not(code):first-child,:not(code):first-child:first-child,:not(code):first-child:last-child *,\
+:not(code):first-child:first-child,:not(code):first-child:first-child:first-child,:not(code):first-child:first-child:last-child *,\
+:not(code):first-child:last-child *,:not(code):first-child:last-child *:first-child,:not(code):first-child:last-child *:last-child *,\
+:not(code):last-child *,:not(code):last-child *:first-child,:not(code):last-child *:last-child *,:not(code):last-child *:first-child,\
+:not(code):last-child *:first-child:first-child,:not(code):last-child *:first-child:last-child *,:not(code):last-child *:last-child *,\
+:not(code):last-child *:last-child *:first-child,:not(code):last-child *:last-child *:last-child *,:not(tt),:not(tt):first-child,\
+:not(tt):last-child *,:not(tt):first-child,:not(tt):first-child:first-child,:not(tt):first-child:last-child *,:not(tt):last-child *,\
+:not(tt):last-child *:first-child,:not(tt):last-child *:last-child *,:not(tt):first-child,:not(tt):first-child:first-child,\
+:not(tt):first-child:last-child *,:not(tt):first-child:first-child,:not(tt):first-child:first-child:first-child,\
+:not(tt):first-child:first-child:last-child *,:not(tt):first-child:last-child *,:not(tt):first-child:last-child *:first-child,\
+:not(tt):first-child:last-child *:last-child *,:not(tt):last-child *,:not(tt):last-child *:first-child,:not(tt):last-child *:last-child *,\
+:not(tt):last-child *:first-child,:not(tt):last-child *:first-child:first-child,:not(tt):last-child *:first-child:last-child *,\
+:not(tt):last-child *:last-child *,:not(tt):last-child *:last-child *:first-child,:not(tt):last-child *:last-child *:last-child *,\
+:not(pre),:not(pre):first-child,:not(pre):last-child *,:not(pre):first-child,:not(pre):first-child:first-child,\
+:not(pre):first-child:last-child *,:not(pre):last-child *,:not(pre):last-child *:first-child,:not(pre):last-child *:last-child *,\
+:not(pre):first-child,:not(pre):first-child:first-child,:not(pre):first-child:last-child *,:not(pre):first-child:first-child,\
+:not(pre):first-child:first-child:first-child,:not(pre):first-child:first-child:last-child *,:not(pre):first-child:last-child *,\
+:not(pre):first-child:last-child *:first-child,:not(pre):first-child:last-child *:last-child *,:not(pre):last-child *,\
+:not(pre):last-child *:first-child,:not(pre):last-child *:last-child *,:not(pre):last-child *:first-child,\
+:not(pre):last-child *:first-child:first-child,:not(pre):last-child *:first-child:last-child *,:not(pre):last-child *:last-child *,\
+:not(pre):last-child *:last-child *:first-child,:not(pre):last-child *:last-child *:last-child *\
+{font-family: ${proportional}};\
+textarea,textarea:first-child,textarea:last-child *,textarea:first-child,textarea:first-child:first-child,\
+textarea:first-child:last-child *,textarea:last-child *,textarea:last-child *:first-child,textarea:last-child *:last-child *,\
+textarea:first-child,textarea:first-child:first-child,textarea:first-child:last-child *,textarea:first-child:first-child,\
+textarea:first-child:first-child:first-child,textarea:first-child:first-child:last-child *,textarea:first-child:last-child *,\
+textarea:first-child:last-child *:first-child,textarea:first-child:last-child *:last-child *,textarea:last-child *,\
+textarea:last-child *:first-child,textarea:last-child *:last-child *,textarea:last-child *:first-child,textarea:last-child *:first-child:first-child,\
+textarea:last-child *:first-child:last-child *,textarea:last-child *:last-child *,textarea:last-child *:last-child *:first-child,\
+textarea:last-child *:last-child *:last-child *,textarea *,textarea *:first-child,textarea *:last-child *,textarea *:first-child,\
+textarea *:first-child:first-child,textarea *:first-child:last-child *,textarea *:last-child *,textarea *:last-child *:first-child,\
+textarea *:last-child *:last-child *,textarea *:first-child,textarea *:first-child:first-child,textarea *:first-child:last-child *,\
+textarea *:first-child:first-child,textarea *:first-child:first-child:first-child,textarea *:first-child:first-child:last-child *,\
+textarea *:first-child:last-child *,textarea *:first-child:last-child *:first-child,textarea *:first-child:last-child *:last-child *,\
+textarea *:last-child *,textarea *:last-child *:first-child,textarea *:last-child *:last-child *,textarea *:last-child *:first-child,\
+textarea *:last-child *:first-child:first-child,textarea *:last-child *:first-child:last-child *,textarea *:last-child *:last-child *,\
+textarea *:last-child *:last-child *:first-child,textarea *:last-child *:last-child *:last-child *,code,code:first-child,\
+code:last-child *,code:first-child,code:first-child:first-child,code:first-child:last-child *,code:last-child *,code:last-child *:first-child,\
+code:last-child *:last-child *,code:first-child,code:first-child:first-child,code:first-child:last-child *,code:first-child:first-child,\
+code:first-child:first-child:first-child,code:first-child:first-child:last-child *,code:first-child:last-child *,code:first-child:last-child *:first-child,\
+code:first-child:last-child *:last-child *,code:last-child *,code:last-child *:first-child,code:last-child *:last-child *,code:last-child *:first-child,\
+code:last-child *:first-child:first-child,code:last-child *:first-child:last-child *,code:last-child *:last-child *,code:last-child *:last-child *:first-child,\
+code:last-child *:last-child *:last-child *,code *,code *:first-child,code *:last-child *,code *:first-child,code *:first-child:first-child,\
+code *:first-child:last-child *,code *:last-child *,code *:last-child *:first-child,code *:last-child *:last-child *,code *:first-child,\
+code *:first-child:first-child,code *:first-child:last-child *,code *:first-child:first-child,code *:first-child:first-child:first-child,\
+code *:first-child:first-child:last-child *,code *:first-child:last-child *,code *:first-child:last-child *:first-child,\
+code *:first-child:last-child *:last-child *,code *:last-child *,code *:last-child *:first-child,code *:last-child *:last-child *,\
+code *:last-child *:first-child,code *:last-child *:first-child:first-child,code *:last-child *:first-child:last-child *,code *:last-child *:last-child *,\
+code *:last-child *:last-child *:first-child,code *:last-child *:last-child *:last-child *,tt,tt:first-child,tt:last-child *,tt:first-child,\
+tt:first-child:first-child,tt:first-child:last-child *,tt:last-child *,tt:last-child *:first-child,tt:last-child *:last-child *,tt:first-child,\
+tt:first-child:first-child,tt:first-child:last-child *,tt:first-child:first-child,tt:first-child:first-child:first-child,\
+tt:first-child:first-child:last-child *,tt:first-child:last-child *,tt:first-child:last-child *:first-child,tt:first-child:last-child *:last-child *,\
+tt:last-child *,tt:last-child *:first-child,tt:last-child *:last-child *,tt:last-child *:first-child,tt:last-child *:first-child:first-child,\
+tt:last-child *:first-child:last-child *,tt:last-child *:last-child *,tt:last-child *:last-child *:first-child,tt:last-child *:last-child *:last-child *,\
+tt *,tt *:first-child,tt *:last-child *,tt *:first-child,tt *:first-child:first-child,tt *:first-child:last-child *,tt *:last-child *,\
+tt *:last-child *:first-child,tt *:last-child *:last-child *,tt *:first-child,tt *:first-child:first-child,tt *:first-child:last-child *,\
+tt *:first-child:first-child,tt *:first-child:first-child:first-child,tt *:first-child:first-child:last-child *,tt *:first-child:last-child *,\
+tt *:first-child:last-child *:first-child,tt *:first-child:last-child *:last-child *,tt *:last-child *,tt *:last-child *:first-child,\
+tt *:last-child *:last-child *,tt *:last-child *:first-child,tt *:last-child *:first-child:first-child,tt *:last-child *:first-child:last-child *,\
+tt *:last-child *:last-child *,tt *:last-child *:last-child *:first-child,tt *:last-child *:last-child *:last-child *,pre,pre:first-child,\
+pre:last-child *,pre:first-child,pre:first-child:first-child,pre:first-child:last-child *,pre:last-child *,pre:last-child *:first-child,\
+pre:last-child *:last-child *,pre:first-child,pre:first-child:first-child,pre:first-child:last-child *,pre:first-child:first-child,\
+pre:first-child:first-child:first-child,pre:first-child:first-child:last-child *,pre:first-child:last-child *,pre:first-child:last-child *:first-child,\
+pre:first-child:last-child *:last-child *,pre:last-child *,pre:last-child *:first-child,pre:last-child *:last-child *,pre:last-child *:first-child,\
+pre:last-child *:first-child:first-child,pre:last-child *:first-child:last-child *,pre:last-child *:last-child *,pre:last-child *:last-child *:first-child,\
+pre:last-child *:last-child *:last-child *,pre *,pre *:first-child,pre *:last-child *,pre *:first-child,pre *:first-child:first-child,\
+pre *:first-child:last-child *,pre *:last-child *,pre *:last-child *:first-child,pre *:last-child *:last-child *,pre *:first-child,\
+pre *:first-child:first-child,pre *:first-child:last-child *,pre *:first-child:first-child,pre *:first-child:first-child:first-child,\
+pre *:first-child:first-child:last-child *,pre *:first-child:last-child *,pre *:first-child:last-child *:first-child,pre *:first-child:last-child *:last-child *,\
+pre *:last-child *,pre *:last-child *:first-child,pre *:last-child *:last-child *,pre *:last-child *:first-child,pre *:last-child *:first-child:first-child,\
+pre *:last-child *:first-child:last-child *,pre *:last-child *:last-child *,pre *:last-child *:last-child *:first-child,pre *:last-child *:last-child *:last-child *\
+{font-family:${monospace}};\
+.post-text ul li,.post-text ol li,.wmd-preview ul li,.wmd-preview ol li{margin-bottom:0}`;
 
 const styleTag = document.createElement("style");
-
-styleTag.innerHTML = `\
-*,:first-child,:last-child *,:first-child,:first-child:first-child,:first-child:last-child *,:last-child *,:last-child :first-child,\
-:last-child :last-child *,:first-child,:first-child:first-child,:first-child:last-child *,:first-child:first-child,:first-child:first-child:first-child\
-,:first-child:first-child:last-child *,:first-child:last-child *,:first-child:last-child :first-child,:first-child:last-child :last-child *,\
-:last-child *,:last-child :first-child,:last-child :last-child *,:last-child :first-child,:last-child :first-child:first-child,
-:last-child :first-child:last-child *,:last-child :last-child *,:last-child :last-child :first-child,:last-child :last-child :last-child *{\
-font-family:${proportional}\
-}textarea,textarea:first-child,textarea:last-child *,textarea:first-child,textarea:first-child:first-child,textarea:first-child:last-child *,\
-textarea:last-child *,textarea:last-child :first-child,textarea:last-child :last-child *,textarea:first-child,textarea:first-child:first-child,\
-textarea:first-child:last-child *,textarea:first-child:first-child,textarea:first-child:first-child:first-child,\
-textarea:first-child:first-child:last-child *,textarea:first-child:last-child *,textarea:first-child:last-child :first-child,\
-textarea:first-child:last-child :last-child *,textarea:last-child *,textarea:last-child :first-child,textarea:last-child :last-child *,\
-textarea:last-child :first-child,textarea:last-child :first-child:first-child,textarea:last-child :first-child:last-child *,\
-textarea:last-child :last-child *,textarea:last-child :last-child :first-child,textarea:last-child :last-child :last-child *,textarea *,\
-textarea :first-child,textarea :last-child *,textarea :first-child,textarea :first-child:first-child,textarea :first-child:last-child *,\
-textarea :last-child *,textarea :last-child :first-child,textarea :last-child :last-child *,textarea :first-child,textarea :first-child:first-child,\
-textarea :first-child:last-child *,textarea :first-child:first-child,textarea :first-child:first-child:first-child,\
-textarea :first-child:first-child:last-child *,textarea :first-child:last-child *,textarea :first-child:last-child :first-child,\
-textarea :first-child:last-child :last-child *,textarea :last-child *,textarea :last-child :first-child,textarea :last-child :last-child *,\
-textarea :last-child :first-child,textarea :last-child :first-child:first-child,textarea :last-child :first-child:last-child *,\
-textarea :last-child :last-child *,textarea :last-child :last-child :first-child,textarea :last-child :last-child :last-child *,code,\
-code:first-child,code:last-child *,code:first-child,code:first-child:first-child,code:first-child:last-child *,code:last-child *,\
-code:last-child :first-child,code:last-child :last-child *,code:first-child,code:first-child:first-child,code:first-child:last-child *,\
-code:first-child:first-child,code:first-child:first-child:first-child,code:first-child:first-child:last-child *,code:first-child:last-child *,\
-code:first-child:last-child :first-child,code:first-child:last-child :last-child *,code:last-child *,code:last-child :first-child,\
-code:last-child :last-child *,code:last-child :first-child,code:last-child :first-child:first-child,code:last-child :first-child:last-child *,\
-code:last-child :last-child *,code:last-child :last-child :first-child,code:last-child :last-child :last-child *,code *,code :first-child,\
-code :last-child *,code :first-child,code :first-child:first-child,code :first-child:last-child *,code :last-child *,code :last-child :first-child,\
-code :last-child :last-child *,code :first-child,code :first-child:first-child,code :first-child:last-child *,code :first-child:first-child,\
-code :first-child:first-child:first-child,code :first-child:first-child:last-child *,code :first-child:last-child *,\
-code :first-child:last-child :first-child,code :first-child:last-child :last-child *,code :last-child *,code :last-child :first-child,\
-code :last-child :last-child *,code :last-child :first-child,code :last-child :first-child:first-child,code :last-child :first-child:last-child *,\
-code :last-child :last-child *,code :last-child :last-child :first-child,code :last-child :last-child :last-child *,tt,tt:first-child,\
-tt:last-child *,tt:first-child,tt:first-child:first-child,tt:first-child:last-child *,tt:last-child *,tt:last-child :first-child,\
-tt:last-child :last-child *,tt:first-child,tt:first-child:first-child,tt:first-child:last-child *,tt:first-child:first-child,\
-tt:first-child:first-child:first-child,tt:first-child:first-child:last-child *,tt:first-child:last-child *,tt:first-child:last-child :first-child,\
-tt:first-child:last-child :last-child *,tt:last-child *,tt:last-child :first-child,tt:last-child :last-child *,tt:last-child :first-child,\
-tt:last-child :first-child:first-child,tt:last-child :first-child:last-child *,tt:last-child :last-child *,tt:last-child :last-child :first-child,\
-tt:last-child :last-child :last-child *,tt *,tt :first-child,tt :last-child *,tt :first-child,tt :first-child:first-child,tt :first-child:last-child *,\
-tt :last-child *,tt :last-child :first-child,tt :last-child :last-child *,tt :first-child,tt :first-child:first-child,tt :first-child:last-child *,\
-tt :first-child:first-child,tt :first-child:first-child:first-child,tt :first-child:first-child:last-child *,tt :first-child:last-child *,\
-tt :first-child:last-child :first-child,tt :first-child:last-child :last-child *,tt :last-child *,tt :last-child :first-child,\
-tt :last-child :last-child *,tt :last-child :first-child,tt :last-child :first-child:first-child,tt :last-child :first-child:last-child *,\
-tt :last-child :last-child *,tt :last-child :last-child :first-child,tt :last-child :last-child :last-child *,pre,pre:first-child,pre:last-child *,\
-pre:first-child,pre:first-child:first-child,pre:first-child:last-child *,pre:last-child *,pre:last-child :first-child,pre:last-child :last-child *,\
-pre:first-child,pre:first-child:first-child,pre:first-child:last-child *,pre:first-child:first-child,pre:first-child:first-child:first-child,\
-pre:first-child:first-child:last-child *,pre:first-child:last-child *,pre:first-child:last-child :first-child,pre:first-child:last-child :last-child *,\
-pre:last-child *,pre:last-child :first-child,pre:last-child :last-child *,pre:last-child :first-child,pre:last-child :first-child:first-child,\
-pre:last-child :first-child:last-child *,pre:last-child :last-child *,pre:last-child :last-child :first-child,pre:last-child :last-child :last-child *,\
-pre *,pre :first-child,pre :last-child *,pre :first-child,pre :first-child:first-child,pre :first-child:last-child *,pre :last-child *,\
-pre :last-child :first-child,pre :last-child :last-child *,pre :first-child,pre :first-child:first-child,pre :first-child:last-child *,\
-pre :first-child:first-child,pre :first-child:first-child:first-child,pre :first-child:first-child:last-child *,pre :first-child:last-child *,\
-pre :first-child:last-child :first-child,pre :first-child:last-child :last-child *,pre :last-child *,pre :last-child :first-child,\
-pre :last-child :last-child *,pre :last-child :first-child,pre :last-child :first-child:first-child,pre :last-child :first-child:last-child *,\
-pre :last-child :last-child *,pre :last-child :last-child :first-child,pre :last-child :last-child :last-child *{\
-font-family:${monospace}}`;
+styleTag.innerHTML = innerHTML;
 
 document.body.appendChild(styleTag);
